35a8a7d25f35e251d58ce40e770d506207362124
Edvin Thörnholm <edvin.thornholm@gmail.com>
2024-10-24 11:29:59 +0200

    cleansed and updated entities

--- ProductProvider.Infrastructure/Data/Contexts/DataContext.cs
+++ ProductProvider.Infrastructure/Data/Contexts/DataContext.cs
@@ -9,10 +9,8 @@ public class DataContext(DbContextOptions<DataContext> options) : DbContext(opti
 {
     public DbSet<CategoryEntity> Categories { get; set; }
     public DbSet<ProductEntity> Products { get; set; }
-    public DbSet<ProductImageEntity> ProductImages { get; set; }
     public DbSet<ReviewEntity> Reviews { get; set; }
-    public DbSet<ProductFilterEntity> ProductFilters { get; set; }
-    public DbSet<ProductToFiltersEntity> ProductToFilters { get; set; }
+
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
@@ -24,21 +22,16 @@ public class DataContext(DbContextOptions<DataContext> options) : DbContext(opti
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<ProductToFiltersEntity>().HasKey(ptf => new { ptf.ProductId, ptf.FilterId });
 
         modelBuilder.Entity<CategoryEntity>().HasKey(c => c.CategoryId);
 
         modelBuilder.Entity<ProductEntity>().HasKey(p => p.ProductId);
         modelBuilder.Entity<ProductEntity>().HasOne(p => p.Category).WithMany(c => c.Products).HasForeignKey(p => p.CategoryId);
-        modelBuilder.Entity<ProductEntity>().HasMany(p => p.Images).WithOne(i => i.Product).HasForeignKey(i => i.ProductId);
         modelBuilder.Entity<ProductEntity>().HasMany(p => p.Reviews).WithOne(r => r.Product).HasForeignKey(r => r.ProductId);
-        modelBuilder.Entity<ProductEntity>().HasMany(p => p.ProductFilters).WithOne(ptf => ptf.Product).HasForeignKey(ptf => ptf.ProductId);
 
-        modelBuilder.Entity<ProductFilterEntity>().HasMany(pf => pf.ProductLinks).WithOne(ptf => ptf.Filter).HasForeignKey(ptf => ptf.FilterId);
 
         base.OnModelCreating(modelBuilder);
     }
 }
 
-//tes
 
