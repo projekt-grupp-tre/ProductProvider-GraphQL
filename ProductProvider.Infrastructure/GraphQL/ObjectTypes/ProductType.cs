4634ad40492d1d6c5f180b1b3e99336de719477b
Marko Misimovic <misimovicmarko@gmail.com>
2024-11-01 09:25:57 +0100

    Lagt till hämta kategorier funktion

--- ProductProvider.Infrastructure/GraphQL/ObjectTypes/ProductType.cs
+++ ProductProvider.Infrastructure/GraphQL/ObjectTypes/ProductType.cs
@@ -1,11 +1,4 @@
-﻿using HotChocolate.Types;
-using ProductProvider.Infrastructure.Data.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace ProductProvider.Infrastructure.GraphQL.ObjectTypes;
 
 using HotChocolate.Types;
